5472e9593b9e857f4d703bc7a54fc195fb5ee0fe
Emmanuel Ruiz <emmanuelre123@outlook.com>
2018-09-29 21:11:06 -0700

    template configurada

--- proyecto/src/BackendBundle/Controller/DefaultController.php
+++ proyecto/src/BackendBundle/Controller/DefaultController.php
@@ -8,6 +8,14 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        //acceso a la bd
+        $em = $this->getDoctrine()->getManager();
+        //obtener identidad user
+        $user_repo = $em->getRepository("BackendBundle:User");
+        $user = $user_repo->find(1);
+        echo "Bienvenido " . $user->getName();
+        var_dump($user);
+        die();
         return $this->render('BackendBundle:Default:index.html.twig');
     }
 }
