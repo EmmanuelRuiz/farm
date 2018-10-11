1a06fe7553d311bd0ea6f6af4fb34939ff57866b
Emmanuel Ruiz <emmanuelre123@outlook.com>
2018-10-11 00:30:22 -0700

    erorr solucionado
    ya muestra la paginacion, aun que sigue en ingles

--- proyecto/src/AppBundle/Controller/AdministratorController.php
+++ proyecto/src/AppBundle/Controller/AdministratorController.php
@@ -7,6 +7,7 @@
  */
 
 namespace AppBundle\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,29 +15,29 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use BackendBundle\Entity\User;
 
-
-
 class AdministratorController extends Controller {
+
     //put your code here
     private $session;
 
     public function __construct() {
         $this->session = new Session();
     }
-    
+
     public function usersAction(Request $request) {
         //para trabajar con la bd hacemos una instancia del entitimanager
         $em = $this->getDoctrine()->getManager();
-        $dql = "SELECT u FROM BackendBundle:User u";
+        $dql = "SELECT u FROM BackendBundle:User u ORDER BY u.id ASC";
         $query = $em->createQuery($dql);
         //ahora usamos el paginador para obtener los registros y poder paginar
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
-                $query, $request->query->getInt('page', 1, 5)
+                $query, $request->query->getInt('page', 1), 5
         );
 
         return $this->render('AppBundle:Administrator:users.html.twig', array(
-            'pagination' => $pagination
+                    'pagination' => $pagination
         ));
     }
+
 }
